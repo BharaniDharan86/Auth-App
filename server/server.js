97b5a7bc5f4e0d664cd1d4a56ce226e3f6691762
bharanidev-001 <bharanidharanm77@gmail.com>
2024-02-08 19:50:45 +0530

    Connect Database

--- server/server.js
+++ server/server.js
@@ -1,4 +1,18 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 const port = 3000;
+const mongoose = require("mongoose");
+
+const DBURL = process.env.DB_URL;
+
+mongoose
+  .connect(DBURL)
+  .then((con) => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log("Error");
+  });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
